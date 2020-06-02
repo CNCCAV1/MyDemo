ed287c9cfb2927d7ed9677184b812bca7a18cd40
何海全 <915528192@qq.com>
2020-06-02 16:29:56 +0800

    创建

---
+++ MyDemo/WpfApp4/Conver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp4
+{
+    [ValueConversion(typeof(int), typeof(Brush))]
+    public class ColorConvert : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Brush brush;
+            if(values == null)
+            {
+                return null;
+            }
+            brush = new SolidColorBrush(Color.FromArgb(System.Convert.ToByte(values[0]), System.Convert.ToByte(values[1]), System.Convert.ToByte(values[2]), System.Convert.ToByte(values[3])));
+            return brush;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyDemo/WpfApp4/ViewModel/MainViewModel.cs
@@ -0,0 +1,89 @@
+using GalaSoft.MvvmLight;
+using System;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace WpfApp4.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                // Code runs in Blend --> create design time data.
+                Title = "²âÊÔ";
+            }
+            else
+            {
+                // Code runs "for real"
+                Title = "±êÌâ";
+                ColorA = 100;
+                colorR = 100;
+                colorG = 100;
+                colorB = 100;
+                //bColor = new SolidColorBrush(Color.FromArgb(Convert.ToByte(ColorA), Convert.ToByte(ColorR), Convert.ToByte(ColorG), Convert.ToByte(ColorB)));
+            }
+        }
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; RaisePropertyChanged(() => Title); }
+        }
+        private int colorA;
+
+        public int ColorA
+        {
+            get { return colorA; }
+            set {
+                colorA = value; RaisePropertyChanged(() => ColorA);
+
+            }
+        }
+        private int colorR;
+
+        public int ColorR
+        {
+            get { return colorR; }
+            set { colorR = value; RaisePropertyChanged(() => ColorR); }
+        }
+        private int colorG;
+
+        public int ColorG
+        {
+            get { return colorG; }
+            set { colorG = value; RaisePropertyChanged(() => ColorG); }
+        }
+        private int colorB;
+
+        public int ColorB
+        {
+            get { return colorB; }
+            set { colorB = value; RaisePropertyChanged(() => ColorB); }
+        }
+        private Brush bColor;
+
+        public Brush BColor
+        {
+            get { return bColor; }
+            set { bColor = value; RaisePropertyChanged(() => BColor); }
+        }
+    }
+}
